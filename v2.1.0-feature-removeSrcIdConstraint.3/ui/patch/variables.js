28d250c6f2ee56e9458130c90b6de45a4fa0f595
s-continuum, IT265 <noreply-jenkins@post.ch>
2024-02-29 15:26:58 +0100

    v2.1.0-feature-removeSrcIdConstraint.3

---
+++ v2.1.0-feature-removeSrcIdConstraint.3/ui/patch/variables.js
@@ -0,0 +1,50 @@
+var variables={
+  "name": "@ch-post/mailpiece-sorting-stream-api",
+  "version": "2.1.0-feature-removeSrcIdConstraint.3",
+  "description": "Mailpiece Sorting Stream API",
+  "scripts": {
+    "start": "apikana start src dist",
+    "stop": "apikana stop",
+    "create-sample": "apikana create-sample",
+    "validate-samples": "apikana validate-samples",
+    "test": "eslint src --ext .ts && apikana validate-samples",
+    "lint": "eslint src --ext .ts",
+    "lint-fix": "eslint src --ext .ts --fix"
+  },
+  "author": "Swiss Post Sorting System Development",
+  "license": "Apache-2.0",
+  "dependencies": {},
+  "devDependencies": {
+    "@typescript-eslint/eslint-plugin": "^4.33.0",
+    "@typescript-eslint/parser": "^4.33.0",
+    "apikana": "^0.9.30",
+    "apikana-defaults": "0.0.77",
+    "eslint": "^7.32.0",
+    "eslint-config-prettier": "^7.1.0",
+    "eslint-plugin-prettier": "^3.4.1",
+    "prettier": "^2.7.1",
+    "typescript": "^4.7.4"
+  },
+  "customConfig": {
+    "type": "stream-api",
+    "domain": "post.ch",
+    "author": "Swiss Post Sorting System Development",
+    "namespace": "logistics.mailpiece.sorting",
+    "shortName": "mailpiece-sorting",
+    "projectName": "mailpiece-sorting-stream-api",
+    "npmPackage": "@ch-post/mailpiece-sorting-stream-api",
+    "title": "Mailpiece Sorting Stream API",
+    "plugins": [
+      "dotnet",
+      "readme"
+    ],
+    "dotnetNamespace": "Ch.Post.Logistics.Mailpiece.Sorting.StreamApi",
+    "dotnetPackageId": "Ch.Post.Logistics.Mailpiece.Sorting.StreamApi",
+    "mqs": "MQTT"
+  },
+  "_": [
+    "start",
+    "src",
+    "dist"
+  ]
+}
